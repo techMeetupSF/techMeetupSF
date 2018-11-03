245f3e719da7a9a2342956778839e5730f4ce50f
wongsitu <sa_waika_4d_23@hotmail.com>
2018-11-03 13:35:57 -0700

    Getting the axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {API} from './env'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        meetups: []
+    }
+  }
+
+  componentDidMount = () =>{
+    axios.get(`https://api.meetup.com/2/open_events?key=${API}&photo-host=public&category=34&status=upcoming&page=3&zip=94102&radius=5&only=name,group.who,group.name,time,event_url,yes_rsvp_count,venue.name,description,venue.address_1,group_topics`)
+      .then(response =>{
+        this.setState({
+          meetups: response.data.results
+        })
+      })
+  }
+
   render() {
+    console.log(this.state.meetups)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi</h1>
       </div>
     );
   }
