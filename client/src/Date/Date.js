5cc70c9d7e5ccabd9bd8917ad0c972cde525cafd
wongsitu <sa_waika_4d_23@hotmail.com>
2018-11-03 16:16:31 -0700

    Flexed

--- client/src/Date/Date.js
+++ client/src/Date/Date.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import './Date.css'
 
 const styles = {
     card: {
@@ -44,7 +44,7 @@ class Date extends Component {
                     </Card>)
         })
         return(
-            <div>
+            <div id='flexing'>
                 {meetupMapped}
             </div>
         );
