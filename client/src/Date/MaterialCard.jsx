b6a04b92461fd0913502f24140402b4679ee0254
niko <jjj.qqqq@gmail.com>
2018-11-05 14:35:30 -0800

    swapping around event card text

--- client/src/Date/MaterialCard.jsx
+++ client/src/Date/MaterialCard.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import './material_card.css';
@@ -20,23 +17,23 @@ class MaterialCard extends Component {
 
     const rsvpCount = event.yes_rsvp_count;
     const memberName = event.group.who;
-    const numberGoing = rsvpCount + " " + memberName + " going.";
+    const numberGoing = rsvpCount + " " + memberName + " going";
 
     const venue = event.venue || '';
-    const hostCompany =  venue.name || 'Unknown Host';
+    const hostCompany =  venue.name || null;
     const timeStamp = event.timeStamp;
-    const hostPlusTime = eventName + " @ " + timeStamp;
-    const description = 'at ' + hostCompany;
+
+    const timeAndLocation = (hostCompany ? hostCompany + " at " + timeStamp : "at " + timeStamp);
     return (
-      <a href={event.event_url} className='not_link'>
+      <a href={event.event_url} className='not_link padded'>
         <Card className='material_card'>
            <CardActionArea className='material_height'>
              <CardContent>
                <Typography gutterBottom variant="h5" component="h2">
-                 {hostPlusTime}
+                 {eventName}
                </Typography>
                <Typography component="p">
-                 {description}
+                 {timeAndLocation}
                </Typography>
                <Typography component="p">
                  {numberGoing}
