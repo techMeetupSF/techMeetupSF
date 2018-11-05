400d62ea4f3d656e4a88c6ab95b4b8db897b6c4f
niko <jjj.qqqq@gmail.com>
2018-11-05 09:36:32 -0800

    meetup api resp, events are sorted by MMDD

---
+++ client/src/helpers/meetup_service.jsx
@@ -0,0 +1,20 @@
+const dayofWeek = (dateStamp) => {
+  return dateStamp.toLocaleDateString([], {weekday: 'long'}).toUpperCase();
+}
+
+const createMMDD = (dateStamp) => {
+  return dateStamp.toLocaleDateString([], { month: '2-digit', day: '2-digit' });
+}
+
+const createTime = (dateStamp) => {
+  return dateStamp.toLocaleDateString([], {hour: '2-digit', minute:'2-digit'});
+}
+
+export const parseTimeIntoDate = (milliseconds) => {
+  const date = new Date(milliseconds);
+  return {
+    dayofWeek: dayofWeek(date),
+    MMDD: createMMDD(date),
+    timeStamp: createTime(date),
+  };
+}
