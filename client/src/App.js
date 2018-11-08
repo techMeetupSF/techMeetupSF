568cec1bc8ab47fa362e919b5a2f10b06d60cc74
niko <jjj.qqqq@gmail.com>
2018-11-08 09:52:01 -0800

    merging branches

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,14 @@ import FilterBar from './FilterBar/FilterBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+<<<<<<< HEAD
 import { parseResults } from './helpers/meetup_service';
 import { showWhichEvents } from './helpers/filter_service';
 import { NUM_EVENTS, NUM_EVENTS_SECOND } from './helpers/constants';
+=======
+import { parseTimeIntoDate, doeshaveDinner, doeshaveFood, doeshavePizza, doeshaveDrinks, doeshaveThirtyRsvp } from './helpers/meetup_service';
+import { showWhichEvents } from './helpers/filter_service';
+>>>>>>> f55b063... Filter bar is working
 
 
 const theme = createMuiTheme({
@@ -42,6 +47,32 @@ class App extends Component {
     this.handleRsvpSwitch = this.handleRsvpSwitch.bind(this);
   }
 
+<<<<<<< HEAD
+=======
+  handleFoodChange(e, showFood) {
+    e.preventDefault();
+    let eventsByDate = this.state.eventsByDate;
+    eventsByDate = showWhichEvents(eventsByDate, this.state.showThirtyPlus, showFood);
+
+    this.setState({
+      showFood,
+      eventsByDate,
+    });
+  };
+
+  handleRsvpSwitch(e, showThirtyPlus) {
+    e.preventDefault();
+    let eventsByDate = this.state.eventsByDate;
+    eventsByDate = showWhichEvents(eventsByDate, showThirtyPlus, this.state.showFood);
+
+    this.setState({
+      showThirtyPlus,
+      eventsByDate,
+    });
+  };
+
+
+>>>>>>> f55b063... Filter bar is working
   componentDidMount = () => {
     const firstFetch = `https://api.meetup.com/2/open_events?key=${API}&photo-host=public&category=34&status=upcoming&page=${NUM_EVENTS}&zip=94102&radius=5&only=name,group.who,group.name,time,event_url,yes_rsvp_count,venue.name,description,venue.address_1,group.id`
     const secondFetch = `https://api.meetup.com/2/open_events?key=${API}&photo-host=public&category=34&status=upcoming&page=${NUM_EVENTS_SECOND}&zip=94102&radius=5&only=name,group.who,group.name,time,event_url,yes_rsvp_count,venue.name,description,venue.address_1,group.id`
